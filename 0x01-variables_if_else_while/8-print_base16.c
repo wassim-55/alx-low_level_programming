285da9ce4254c080cba81f66b564ba30ffb4b63c
wassim-55 <wchahed55@gmail.com>
2023-06-18 10:14:00 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		if (digit < 57 || digit >= 96)
+			putchar(digit);
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
