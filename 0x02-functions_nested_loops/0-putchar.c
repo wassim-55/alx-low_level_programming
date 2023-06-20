647b1da9634bf1a17f52427493834710c0d45e61
wassim-55 <wchahed55@gmail.com>
2023-06-20 07:33:46 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+
+	for (int i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
