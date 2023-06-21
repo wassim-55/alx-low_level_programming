2ca21e7a7569c5f4fbd3ef857710088c2e6d6365
wassim-55 <wchahed55@gmail.com>
2023-06-20 18:55:50 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include"main.h"
+/**
+ * print_sign - print + if n is greater than zero
+ *		0 if n equal to zero
+ *		- if n less than zero
+ *
+ *@n: input from function
+ *
+ *Return: 1 if +, 0 if 0 and -1 if -
+ *
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
