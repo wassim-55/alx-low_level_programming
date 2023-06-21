255045df9d489c0012b80e76183a9cccc8013d2a
wassim-55 <wchahed55@gmail.com>
2023-06-21 12:05:27 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (i = 0 ; i < 50; i++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1  = fib2;
+		fib2 = sum;
+
+		if (i == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
+
+
+
