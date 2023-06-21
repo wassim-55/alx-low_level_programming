7145aa311bf1741b7afdfa89d483593b80369dd4
wassim-55 <wchahed55@gmail.com>
2023-06-21 09:58:07 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,14 +24,14 @@ void print_times_table(int n)
 
 				k = i * j;
 
-				if (prod <= 9)
+				if (k <= 9)
 					_putchar(' ');
-				if (prod <= 99)
+				if (k <= 99)
 					_putchar(' ');
-				if (prod >= 100)
+				if (k >= 100)
 				{
-					_putchar((prod / 100) + 48);
-					_putchar((prod / 10) % 10 + 48);
+					_putchar((k / 100) + 48);
+					_putchar((k / 10) % 10 + 48);
 				} else if (k <= 99 && k >= 10)
 					_putchar((k / 10) + 48);
 				_putchar((k % 10) + 48);
