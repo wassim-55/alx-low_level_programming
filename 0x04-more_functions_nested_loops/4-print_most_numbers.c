89b4f9337ece3d4656ace09ab9e2c374d5699e8b
wassim-55 <wchahed55@gmail.com>
2023-06-23 18:16:57 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,9 +13,8 @@ void print_most_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		if (num == 2 || num == 4)
-			continue;
-		_putchar(num + 48);
+		if (!(num == 2 || num == 4))
+			_putchar(num + 48);
 	}
 	_putchar('\n');
 }
