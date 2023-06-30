faae9b121f1e18682e64548160b74188d7c14311
wassim-55 <wchahed55@gmail.com>
2023-06-29 17:48:03 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,61 +1,34 @@
 #include "main.h"
-#include <stdio.h>
 /**
- *islower - determinate whether ASCII is lowercasa
- *@c: character
- *Return: 1 if true, 0 if false
- *
+ * cap_string - capitalizes all words of a string
+ *@str: input
+ *Return: a pointer that changes string
 */
 
-int islower(char c)
+char *cap_string(char *str)
 {
-	return (c >= 97 && c <= 122);
-}
+	int index = 0;
 
-/**
- *isdelimiter - determinate whether ascii is a delimiter
- *@c: character
- *Return: 1 if true, 0 if false
-*/
-
-int isdelimiter(char c)
-{
-	int i;
-	char delimiter[] = " \t\n,.!?\"(){}";
-
-	for (i = 0; i < 12; i++)
+	while (str[index])
 	{
-		if (c == delimiter[i])
-			return (1);
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+		if (str[index - 1] == ' ' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+			str[index] -= 32;
+		index++;
 	}
-	return (0);
-}
-
-/**
- *cap_string - capitalization all words of a string
- *
- *@s: input
- *Return: string
- *
-*/
-char *cap_string(char *s)
-{
-	char *ptr = s;
-	int founddelimit = 1;
-
-	while (*s)
-	{
-		if (isdelimiter(*s))
-			founddelimit = 1;
-		else if (islower(*s) && founddelimit)
-		{
-			*s -= 32;
-			founddelimit = 0;
-		}
-		else
-			founddelimit = 0;
-		s++;
-	}
-	return (ptr);
 }
 
