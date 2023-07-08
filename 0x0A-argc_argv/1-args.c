45f0cad2ad6b6f88b6d6f0f2a59b305f7e5ab191
wassim-55 <wchahed55@gmail.com>
2023-07-07 19:38:08 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,37 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include "main.h"
 /**
  * main - Entry point
- *@argc: int
+ * @argc: int
  *@argv: int
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	if (argc == 2)
-	{
-		int i, lcents = 0, money = atoi(argv[1]);
-		int cents[] = {25, 10, 5, 2, 1}
+	(void)argv;
+	printf("%d\n", argc - 1);
 
-		for (i = 0; i < 5; i++)
-		{
-			if (money >= cents[i])
-			{
-				lcents += money / cents[i];
-				money = money % cents[i];
-				if (money % cents[i] == 0)
-				{
-					break;
-				}
-			}
-		}
-		printf("%d\n", lcents);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
 	return (0);
 }
---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *@argc: int
+ *@argv: int
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i, lcents = 0, money = atoi(argv[1]);
+		int cents[] = {25, 10, 5, 2, 1}
+
+		for (i = 0; i < 5; i++)
+		{
+			if (money >= cents[i])
+			{
+				lcents += money / cents[i];
+				money = money % cents[i];
+				if (money % cents[i] == 0)
+				{
+					break;
+				}
+			}
+		}
+		printf("%d\n", lcents);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+
+}
