5d5bb54eb0b524f6893521cc707bc992384b55fc
wassim-55 <wchahed55@gmail.com>
2023-07-11 08:54:23 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - contains a copy of the string given as a parameter
+ *
+ *@str: string
+ *
+ *Return: copied string
+*/
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *m;
+
+	if (str == NULL)
+		return (NULL);
+	for (; str[size] != '\0'; size++)
+		;
+	m = malloc(size * sizeof(*str) + 1);
+	if (m == 0)
+		return (NULL);
+	for (; i < size; i++)
+		m[i] = str[i];
+
+	return (m);
+}
+
