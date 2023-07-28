956c17d1a24a742157e452b2c006178343ce8e2e
wassim-55 <wchahed55@gmail.com>
2023-07-27 19:07:17 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+void _fn(void)__attribute__((fn));
+
+/**
+ *_fn - executes before main
+ *
+ *Return: void
+ *
+ *
+*/
+void _fn(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
