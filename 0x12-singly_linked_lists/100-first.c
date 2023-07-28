7873c37e293056790bd5e10952d343a93c7c691c
wassim-55 <wchahed55@gmail.com>
2023-07-27 19:12:23 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,13 @@
-#include "lists.h"
-void _fn(void)__attribute__((fn));
+#include <stdio.h>
 
 /**
- *_fn - executes before main
+ * main - check the code
  *
- *Return: void
- *
- *
-*/
-void _fn(void)
+ * Return: Always 0.
+ */
+int main(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-		"I bore my house upon my back!\n");
+	printf("(A tortoise, having pretty good sense of a hare's nature,
+		challenges one to a race.)\n");
+	return (0);
 }
