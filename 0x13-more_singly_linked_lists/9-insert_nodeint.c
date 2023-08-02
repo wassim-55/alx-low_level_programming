2f3157d6d539c2d4640016977f7adc18d3059372
wassim-55 <wchahed55@gmail.com>
2023-08-01 17:48:08 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,13 +24,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next == NULL;
 
-	if (!idx)
+	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-	while (current)
+	for (index = 0; current && index < idx; index++)
 	{
 		if (index == idx - 1)
 		{
@@ -38,10 +38,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			current->next = new_node;
 			return (new_node);
 		}
-		index++;
-		current = current->next;
+		else
+			current = current->next;
 	}
-
-	free(new_node);
 	return (NULL);
 }
