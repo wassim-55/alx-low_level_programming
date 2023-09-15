9f25a0eec0c850463d350ac6ae3e0d46a2a5f73d
wassim-55 <wchahed55@gmail.com>
2023-09-14 18:23:33 -0700

    linkedlists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,4 +32,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				if (head->next == NULL)
 					new = add_dnodeint_end(h, n);
 				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
+	}
 
+	return (new);
+}
